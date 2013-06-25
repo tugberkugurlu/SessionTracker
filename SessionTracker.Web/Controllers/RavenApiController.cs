cb38fb111d26557f1311b50af4205b4097b8d9c0
tugberkugurlu <tugberkugurlu@gmail.com>
2013-06-25 09:30:20 +0300

    formatting changes

--- SessionTracker.Web/Controllers/RavenApiController.cs
+++ SessionTracker.Web/Controllers/RavenApiController.cs
@@ -5,19 +5,19 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 
-namespace SessionTracker.Web.Controllers {
-
-    public abstract class RavenApiController : ApiController {
-
+namespace SessionTracker.Web.Controllers
+{
+    public abstract class RavenApiController : ApiController
+    {
         protected readonly IAsyncDocumentSession RavenSession;
 
-        protected RavenApiController(IAsyncDocumentSession documentSession) {
-
+        protected RavenApiController(IAsyncDocumentSession documentSession)
+        {
             RavenSession = documentSession;
         }
 
-        public async override Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken) {
-            
+        public async override Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken)
+        {
             HttpResponseMessage response = await base.ExecuteAsync(controllerContext, cancellationToken);
             await RavenSession.SaveChangesAsync();
 
